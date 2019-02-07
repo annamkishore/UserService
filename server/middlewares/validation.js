c43e296c344ec3e8cee25aca7e8e5e0b591cf4c9
Kishore Annam <annamkishore@gmail.com>
2019-02-07 22:10:13 +0530

    User Service: validation

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -1,13 +1,38 @@
-function validateLogin() {
-  console.log('validating login');
-}
-
-function validateRegistration() {
-  console.log('validating registration');
-}
-
+const Joi = require('joi');
+const log = require('../logger');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+
+const loginSchema = {
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+};
+
+const registerSchema = {
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  firstname: Joi.string().min(2).max(30).required(),
+  lastname: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email(),
+};
+
+function validateLogin(req, res, next) {
+  let {username, password} = req.body;
+
+  Joi.validate({username, password}, loginSchema, (err, value) => {
+    err && log.error(err);
+    err ? res.status(400).json({message: 'Invalid Login Input!'}) : next();
+  });
+}
+
+function validateRegistration(req, res, next) {
+  let user = req.body;
+
+  Joi.validate(user, registerSchema, (err, value) => {
+    err && log.error(err);
+    err ? res.status(400).json({message: 'Invalid Registration Input!'}) : next();
+  });
+}
 
 function validateToken(req, res, next) {
   let token = req.headers['x-access-token'] || req.headers['authorization'];
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+const validation = require('../middlewares/validation');
 
-router.post('/login', userController.login);
-router.post('/register', userController.register);
+router.post('/login', validation.validateLogin, userController.login);
+router.post('/register', validation.validateRegistration, userController.register);
 
 module.exports = router;
