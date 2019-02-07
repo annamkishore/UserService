d45dad7bbfff362b057906809f1bd1b4e3c79bfe
Kishore Annam <annamkishore@gmail.com>
2019-02-07 21:23:42 +0530

    User Service: unit test completion

--- server/tests/userController.test.js
+++ server/tests/userController.test.js
@@ -1,44 +1,48 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../index');
-const mocha = require('mocha');
+const app = require('../index');
 
-describe('UserService Backend App unittest', () => {
+describe('UserService: Backend App unittest', () => {
   chai.use(chaiHttp);
   const expect = chai.expect;
-  const agent = chai.request(server);
+  const agent = chai.request.agent(app).keepOpen();
 
-  before((done) => {
-    setTimeout(() => done(), 5000);
+  before((done) => setTimeout(() => done(), 5000));
+  after(done => {
+    done();
+    process.exit(0);
+    // agent.close(); // not closing somehow
   });
 
-  it('Registration & Login test', (done) => {
-    let user = {
-      "username": "kishoreannam",
-      "firstname": "kishore",
-      "lastname": "annam",
-      "email": "kishore@gmail.com",
-      "password": "hello123",
-    };
+  describe('Registration & Login', () => {
+    it('should register the given user', (done) => {
+      let user = {
+        "username": "kishoreannam",
+        "firstname": "kishore",
+        "lastname": "annam",
+        "email": "kishore@gmail.com",
+        "password": "hello123",
+      };
 
-    agent
-      .post('/user/register')
-      .set('Content-Type', 'application/json')
-      .send(user)
-      .end((req, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+      agent
+        .post('/user/register')
+        .set('Content-Type', 'application/json')
+        .send(user)
+        .end((req, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
 
-    agent
-      .post('/user/login')
-      .set('Content-Type', 'application/json')
-      .send({username: 'kishoreannam', password: 'hello123'})
-      .end((req, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+    it('should login with the given user', (done) => {
+      agent
+        .post('/user/login')
+        .set('Content-Type', 'application/json')
+        .send({username: 'kishoreannam', password: 'hello123'})
+        .end((req, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
   });
 });
-
-
